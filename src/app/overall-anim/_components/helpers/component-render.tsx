26c4692df11f0e3c504abec08d3df541b3cbc9da
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2024-09-26 23:14:12 +0530

    Feat/framer basics (#4)
    * trying to do some animations
    
    * fix structure
    
    * completed framer basics #2

--- src/app/overall-anim/_components/helpers/component-render.tsx
+++ src/app/overall-anim/_components/helpers/component-render.tsx
@@ -1,6 +1,8 @@
 import Basics from "../basics";
 import Controls from "../controls";
 import Gestures from "../gestures";
+import ScrollBased from "../scroll-based";
+import ViewBased from "../view-based";
 
 type Props = { selectedComponent: string };
 
@@ -12,6 +14,10 @@ export default function ComponentRenderer({ selectedComponent }: Props) {
       return <Gestures />;
     case "Controls":
       return <Controls />;
+    case "ViewBased":
+      return <ViewBased />;
+    case "ScrollBased":
+      return <ScrollBased />;
     default:
       return null;
   }
---
+++ src/app/overall-anim/_components/view-based.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRef } from "react";
+import { Layout } from "./helpers/layout";
+import { motion, useInView } from "framer-motion";
+
+export default function ViewBased() {
+  const ref = useRef(null);
+
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <Layout title="ViewBased">
+      <div className="h-[200vh] border w-full flex" />
+      <motion.div
+        className="h-screen border w-full bg-white"
+        initial={{ opacity: 0, y: 200 }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{ duration: 1 }}
+      />
+      <div
+        ref={ref}
+        className={`h-screen border w-full ${isInView ? "bg-red-400" : "bg-blue-400"} duration-1000 transition`}
+      />
+    </Layout>
+  );
+}
