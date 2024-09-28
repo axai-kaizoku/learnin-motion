35c3267fdeee80734e0e014b0b6b65fa7742b3d2
Akshay Yelle <02b3akshay@gmail.com>
2024-09-28 15:33:11 +0530

    add horizontal scroll #3

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { ToggleTheme } from "../toggle-theme";
 
-const navLinks = ["/on-scroll-reveal", "/overall-anim"];
+const navLinks = ["/on-scroll-reveal", "/overall-anim", "/horizontal-scroll"];
 
 export default function Sidebar() {
   return (
