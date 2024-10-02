2ef79789bb81eb13d3cafeccd6b6648141c8790a
Akshay Yelle <02b3akshay@gmail.com>
2024-10-02 17:19:59 +0530

    fix shad type error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,10 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  // You can add custom props here in the future
+  foo?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
